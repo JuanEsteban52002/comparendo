2c272e331ad004a316d1b36d7f8a7de5e61a8250
JUANESTEBAN <juane.grandar@uqvirtual.edu.co>
2022-09-20 19:44:10 -0500

    Mejoras en el front
    Se avanzó en el desarrollo del front, se agregaron más componentes, está aproximadamente al 70% a nivel de front

--- web/src/main/java/co/edu/uniquindio/comparendo/bean/ComparendoBean.java
+++ web/src/main/java/co/edu/uniquindio/comparendo/bean/ComparendoBean.java
@@ -1,10 +1,15 @@
 package co.edu.uniquindio.comparendo.bean;
 
+import co.edu.uniquindio.comparendo.entidades.Comparendo;
 import co.edu.uniquindio.comparendo.servicios.ComparendoServicio;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
 
@@ -12,11 +17,28 @@ import java.io.Serializable;
 @ViewScoped
 public class ComparendoBean implements Serializable {
 
-    @Autowired
+    @Getter @Setter
+    private Comparendo comparendo;
+
+
     private ComparendoServicio comparendoServicio;
 
     @PostConstruct
-    public void inicializar(){
+    public void inicializar() throws Exception {
+        comparendo = new Comparendo();
 
+    }
+
+    public String guardarComparendo(){
+        try{
+
+            comparendoServicio.guardarComparendo(comparendo);
+
+        }catch(Exception e){
+            FacesMessage msj = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alerta", e.getMessage());
+            FacesContext.getCurrentInstance().addMessage("msj_bean", msj);
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return null;
     }
 }
